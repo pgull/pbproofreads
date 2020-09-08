bfe0a81be15f40ee9faf8e15298feb2e6c2fbaa7
Preston Gull <pgull@Prestons-MacBook-Pro.local>
2020-09-07 20:30:54 -0600

    preview working

--- src/router/index.js
+++ src/router/index.js
@@ -11,17 +11,15 @@ const routes = [
     component: Home
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    path: "/preview",
+    name: "Preview",
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+      import("../views/Preview.vue")
   }
 ];
 
 const router = new VueRouter({
+  mode: 'history',
   routes
 });
 

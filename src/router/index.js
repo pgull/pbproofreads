3fb36f4916c8749a1f185bdf18dd36f2c1470188
Preston Gull <pgull@Prestons-MacBook-Pro.local>
2020-09-07 20:42:00 -0600

    halp

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
-import Home from "../views/Preview.vue";
+import Preview from "../views/Preview.vue";
 
 Vue.use(VueRouter);
 

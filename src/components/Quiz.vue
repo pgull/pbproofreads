58247960cc2bfa56efa248fd76be9e95387e4d53
Preston Gull <pgull@Prestons-MacBook-Pro.local>
2020-11-04 19:07:16 -0700

    forms

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -12,7 +12,8 @@
   <div v-show="questionIndex === this.questions.length">
     <h2>Let's get in touch.</h2>
     <h3> {{ this.rating < 4 ? 'This proofread could be free for you.' : this.rating < 8 ? 'This proofread could be as little as $0.01 per word.' : 'We need more information.' }} </h3>
-    <form name="pb-contact" method="POST" data-netlify="true">
+    <form name="pb-contact" method="POST" data-netlify="true" method="POST"  name="contactform" data-netlify="true"  data-netlify-honeypot="bot-field" enctype="application/x-www-form-urlencoded">
+      <input type="hidden" name="pb-contact-form" value="quote" />
       <h4>Your name:</h4>
       <input type="text" name="name" placeholder="John Doe" />
       <h4>Your email:</h4>
